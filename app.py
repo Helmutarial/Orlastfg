8b70a9ccd2075f738dc452b08eac95091b1d3d19
Pedro Gutierrez <pedro.gutierrez.hernando@alumnos.upm.es>
2024-09-25 18:41:24 +0200

    puede que este mal

--- app.py
+++ app.py
@@ -116,7 +116,8 @@ def index():
 def logout():
     id_token = session.get('oidc_auth_token').get('id_token')
     session.clear()
-    return redirect("https://sso.dat.etsit.upm.es/realms/tfg-orlas/protocol/openid-connect/logout?id_token_hint=%s&post_logout_redirect_uri=%s" % (id_token, urllib.parse.quote("https://orlas.eurielec.etsit.upm.es/inicio", safe='')))
+    #return redirect("https://sso.dat.etsit.upm.es/realms/tfg-orlas/protocol/openid-connect/logout?id_token_hint=%s&post_logout_redirect_uri=%s" % (id_token, urllib.parse.quote("https://orlas.eurielec.etsit.upm.es/inicio", safe='')))
+    return redirect("https://sso.dat.etsit.upm.es/realms/tfg-orlas/protocol/openid-connect/logout?id_token_hint=%s&post_logout_redirect_uri=%s" % (id_token, urllib.parse.quote("http://127.0.0.1:5000/inicio", safe='')))
 
 @app.route('/private') #la ruta se tiene que llamar asi, si no no funciona
 @oidc.require_login
